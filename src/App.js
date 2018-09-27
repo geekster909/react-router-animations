517e38f2ca6ed9c6b323ab240cfc5bb6146f974e
Justin Bond <bond.justink@gmail.com>
2018-09-27 15:53:42 -0700

    route transitions

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
+// ./src/App.js
+import { Route } from 'react-router-dom';
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Subscribe from './pages/Subscribe'
+import ThankYou from './pages/ThankYou'
+import PageShell from './components/PageShell'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route path="/" exact component={PageShell(Subscribe)}></Route>
+        <Route path="/thanks" exact component={PageShell(ThankYou)}></Route>
       </div>
     );
   }
 }
-
 export default App;
